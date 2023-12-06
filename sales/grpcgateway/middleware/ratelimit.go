42645348dd0cc0301f4080036ac98178734b26b4
ramseyjiang <ramseyjiang@gmail.com>
2023-12-06 21:57:47 +1300

    update ratelimit

--- sales/grpcgateway/middleware/ratelimit.go
+++ sales/grpcgateway/middleware/ratelimit.go
@@ -7,12 +7,18 @@ import (
 )
 
 const (
-	interval  = 1
-	frequency = 5
+	requestsPerMinute = 30
+	intervalInSeconds = 60 // 60 seconds in a minute
 )
 
+// Calculate the rate as requests per second
+var ratePerSecond = rate.Limit(requestsPerMinute / intervalInSeconds)
+
+// The burst size is the maximum number of requests allowed in a single burst, set to 5
+var burstSize = 5
+
 // Create a rate limiter instance (adjust the parameters as per your requirements)
-var limiter = rate.NewLimiter(interval, frequency) // 1 request per second with a burst of 5
+var limiter = rate.NewLimiter(ratePerSecond, burstSize) // 1 request per second with a burst of 5
 
 // RateLimit enforces rate limiting on incoming requests using build-in library.
 func RateLimit(next http.Handler) http.Handler {
